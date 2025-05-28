95c5b4af477a45e8b6cca27b3a15de1df774098b
LexieLiu19 <xuliu0925@gmail.com>
2025-05-28 15:23:06 -0400

    edit styles

--- react_homework/src/day14_practice/TicTacToe/mySolution/Board.jsx
+++ react_homework/src/day14_practice/TicTacToe/mySolution/Board.jsx
@@ -3,18 +3,13 @@ import Square from './Square';
 export default function Board({ squares, onSquareClick }) {
   const renderSquare = (i) => {
     return (
-      <Square
-        // className="row"
-        key={i}
-        value={squares[i]}
-        onClick={() => onSquareClick(i)}
-      />
+      <Square key={i} value={squares[i]} onClick={() => onSquareClick(i)} />
     );
   };
   return (
     <div className="board">
       {[0, 3, 6].map((row) => (
-        <div key={row}>
+        <div key={row} className="row">
           {renderSquare(row)}
           {renderSquare(row + 1)}
           {renderSquare(row + 2)}
